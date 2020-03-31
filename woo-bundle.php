a99626a5438cedb3afc41361856b654a69f92ada
Tanvirul Haque <tanvirul6@users.noreply.github.com>
2020-03-31 15:09:20 +0600

    Register Product Type

--- woo-bundle.php
+++ woo-bundle.php
@@ -5,3 +5,17 @@ Description: A simple woocommerce bundle product plugin
 Author: N/A
 Version: 1.0
 */
+
+
+/**
+ * Register Product Type
+ */
+function wcbp_register_bundle_product_type() {
+	class WC_Product_bundle_product extends WC_Product {
+		public function __construct( $product ) {
+			$this->product_type = 'bundle_product';
+			parent::__construct( $product );
+		}
+	}
+}
+add_action('init', 'wcbp_register_bundle_product_type');
